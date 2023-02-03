29a00c63fb2fc27d488d93e74d15852a0b116a79
Frozen214 <atema595@gmail.com>
2023-02-04 01:09:18 +0300

    ExportDataTableWord
    using a datatable to export to a word.

---
+++ ExportDataTableWord.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string con = @"Data Source=user-pc\sqlexpress;Initial Catalog=Remote-Acc;Integrated Security=True";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var query = "select Действие, Дата from ЛогМониторинга where Код_исполнителя = 96";
+
+            var datatable = new DataTable();
+
+            queryReturnData(query, datatable);
+            ExportToWord(datatable);
+        }
+        public DataTable queryReturnData(string query, DataTable dataTable)
+        {
+            SqlConnection myCon = new SqlConnection(con);
+            myCon.Open();
+
+            SqlDataAdapter SDA = new SqlDataAdapter(query, myCon);
+            SDA.SelectCommand.ExecuteNonQuery();
+
+            SDA.Fill(dataTable);
+            return dataTable;
+        }
+        private void ExportToWord(DataTable dataTable)
+        {
+            if (dataTable.Rows.Count > 0)
+            {
+                Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+                word.Application.Documents.Add(Type.Missing);
+
+                Microsoft.Office.Interop.Word.Table table = word.Application.ActiveDocument.Tables.Add(word.Selection.Range, dataTable.Rows.Count + 1, dataTable.Columns.Count, Type.Missing, Type.Missing);
+                table.Borders.OutsideLineStyle = Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
+                table.Borders.InsideLineStyle = Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
+
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    table.Cell(1, i + 1).Range.Text = dataTable.Columns[i].ColumnName;
+                }
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataTable.Columns.Count; j++)
+                    {
+                        table.Cell(i + 2, j + 1).Range.Text = dataTable.Rows[i][j].ToString();
+                    }
+                }
+
+                word.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("No data to export!");
+            }
+        }
+    }
+}

3cf03aa15fc0cb3c274327d7a81f5c17ccac3904
Frozen214 <atema595@gmail.com>
2023-03-19 00:52:49 +0300

    Create ListBoxLoadElement.cs
    Uploading data to a listbox from MS SQL Server

---
+++ ListBoxLoadElement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string stringCon()
+        {
+            return @"Data Source=user-pc\sqlexpress;Initial Catalog=JmutovDB;Integrated Security=True";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var queryListCodeRequest = "SELECT * FROM Пользователи";
+            loadElementToComboBox(queryListCodeRequest, "Логин", comboBox1);
+        }
+        public void loadElementToComboBox(string stringQuery, string column, ComboBox myBox)
+        {
+            List<string> columnValues = GetColumnValues(stringQuery, column);
+            myBox.Items.AddRange(columnValues.ToArray());
+        }
+        public List<string> GetColumnValues(string query, string columnName)
+        {
+            List<string> columnValues = new List<string>();
+
+            SqlConnection myCon = new SqlConnection(stringCon());
+            myCon.Open();
+            using (SqlCommand command = new SqlCommand(query, myCon))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    object columnValueObject = reader.GetValue(reader.GetOrdinal(columnName));
+                    string columnValue = columnValueObject != DBNull.Value ? columnValueObject.ToString() : "";
+                    columnValues.Add(columnValue);
+                }
+            }
+            return columnValues;
+        }
+    }
+}

1218988f467c37c98db3fdfd35215eb4c9fcf90c
Frozen214 <atema595@gmail.com>
2023-07-06 23:47:49 +0300

    add, delete, update sql query C#

---
+++ Add-Delete-update/DB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public class DB
+    {
+        public string StringCon()
+        {
+            return @"Data Source=USER-PC\SQLEXPRESS;Initial Catalog=CarShopTG;Integrated Security=True";
+        }
+        public SqlDataAdapter queryExecute(string query)
+        {
+            try
+            {
+                SqlConnection myCon = new SqlConnection(StringCon());
+                myCon.Open();
+
+                SqlDataAdapter SDA = new SqlDataAdapter(query, myCon);
+
+                SDA.SelectCommand.ExecuteNonQuery();
+                MessageBox.Show("Действие успешно выполнено!", "Успех");
+                return SDA;
+            }
+            catch
+            {
+                MessageBox.Show("Возникла ошибка при выполнении запроса.", "Ошибка");
+                return null;
+            }
+        }
+    }
+}
---
+++ Add-Delete-update/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class Form1 : Form
+    {
+        private DB db;
+        public Form1()
+        {
+            db = new DB(); 
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var queryAddMark= $"insert into Марка (Наименование) values ('{textBox1.Text}')";
+            db.queryExecute(queryAddMark);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var queryAddMark = $"delete Марка where КодМарки = {textBox2.Text}";
+            db.queryExecute(queryAddMark);
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            var queryAddMark = $"update Марка set Наименование = '{textBox3.Text}' where КодМарки = {textBox4.Text}";
+            db.queryExecute(queryAddMark);
+        }
+    }
+}

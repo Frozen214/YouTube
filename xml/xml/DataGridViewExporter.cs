504586cfc86c566814b2f68d7a49b65cc86ac202
Frozen214 <atema595@gmail.com>
2023-11-21 18:53:38 +0300

    XML-READ-WRITE

---
+++ xml/xml/DataGridViewExporter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+namespace xml
+{
+    public class DataGridViewExporter
+    {
+        public void ExportToXml(DataGridView dataGridView)
+        {
+            string projectPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            string exportFolderPath = Path.Combine(projectPath, "xmlExport");
+            Directory.CreateDirectory(exportFolderPath);
+
+            string fileName = $"Xml_{DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss")}";
+            string filePath = Path.Combine(exportFolderPath, fileName);
+
+            XmlDocument xmlDocument = new XmlDocument();
+
+            XmlElement dataElement = xmlDocument.CreateElement("Data");
+
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                XmlElement rowElement = xmlDocument.CreateElement("Row");
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    XmlElement cellElement = xmlDocument.CreateElement(dataGridView.Columns[cell.ColumnIndex].HeaderText);
+                    cellElement.InnerText = cell.Value != null ? cell.Value.ToString() : string.Empty;
+                    rowElement.AppendChild(cellElement);
+                }
+                dataElement.AppendChild(rowElement);
+            }
+            xmlDocument.AppendChild(dataElement);
+
+            try
+            {
+                xmlDocument.Save(filePath + ".xml");
+                MessageBox.Show("Экспорт данных в XML выполнен успешно.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при экспорте данных в XML: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ xml/xml/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace xml
+{
+    public partial class FormExampleXML : Form
+    {
+        public FormExampleXML()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            var exportXml = new DataGridViewExporter();
+            exportXml.ExportToXml(guna2DataGridView1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            guna2DataGridView1.Rows.Add("4234", "Молоко", "70");
+            guna2DataGridView1.Rows.Add("1314", "Печенье", "60");
+            guna2DataGridView1.Rows.Add("1984", "Хлеб", "40");
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML файлы (*.xml)|*.xml";
+            openFileDialog.Title = "Выберите XML файл";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                var importXml = new ImportXml();
+                importXml.LoadXmlDataToDataGridView(filePath, guna2DataGridView2);
+            }
+        }
+    }
+}
+

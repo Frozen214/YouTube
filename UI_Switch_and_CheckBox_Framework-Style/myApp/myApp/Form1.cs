fd2dd33e81d95cf1f6103aa97555d5a859000992
Frozen214 <atema595@gmail.com>
2023-12-27 22:43:48 +0300

    Первичная загрузка проекта
    Стильные переключатели и чекБоксы Win Form

---
+++ UI_Switch_and_CheckBox_Framework-Style/myApp/myApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using MaterialSkin.Controls;
+using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myApp
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Green400, Primary.Green600,
+                Primary.Blue500, Accent.Orange400,
+                TextShade.WHITE
+                );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialSwitch3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
